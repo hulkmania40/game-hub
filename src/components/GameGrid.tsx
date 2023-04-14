2ccdfbb9b9d8beae2f9ec89092ca2732bfef3e79
Mubashir Ali <ali.mubashir40@gmail.com>
2023-04-14 20:33:02 +0530

    Adding Platform Icons

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,23 +1,24 @@
-import {SimpleGrid, Text} from "@chakra-ui/react"
-import useGames from "../hooks/useGames"
-import GameCard from "./GameCard"
-
-
+import { SimpleGrid, Text } from "@chakra-ui/react";
+import useGames from "../hooks/useGames";
+import GameCard from "./GameCard";
 
 const GameGrid = () => {
-  
-    const {games,error} = useGames()
+  const { games, error } = useGames();
 
-    return (
-        <>
-            {error && <Text>{error}</Text>}
-            <SimpleGrid columns={{sm:1, md:2, lg: 3, xl: 5}} padding='10px' spacing={10}>
-                {games.map((game) => 
-                    <GameCard key={game.id} game={game} />
-                )}
-            </SimpleGrid>
-        </>
-  )
-}
+  return (
+    <>
+      {error && <Text>{error}</Text>}
+      <SimpleGrid
+        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
+        padding="10px"
+        spacing={10}
+      >
+        {games.map((game) => (
+          <GameCard key={game.id} game={game} />
+        ))}
+      </SimpleGrid>
+    </>
+  );
+};
 
-export default GameGrid
+export default GameGrid;
---
+++ src/components/PlatformIconsList.tsx
@@ -0,0 +1,43 @@
+import {
+  FaWindows,
+  FaPlaystation,
+  FaXbox,
+  FaApple,
+  FaAndroid,
+  FaLinux,
+} from "react-icons/fa";
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
+import { Platform } from "../hooks/useGames";
+import { HStack, Icon } from "@chakra-ui/react";
+
+import { IconType } from "react-icons";
+
+interface Props {
+  platforms: Platform[];
+}
+
+const PlatformIconsList = ({ platforms }: Props) => {
+  const iconMap: { [key: string]: IconType } = {
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    xbox: FaXbox,
+    android: FaAndroid,
+    mac: FaApple,
+    ios: MdPhoneIphone,
+    linux: FaLinux,
+    web: BsGlobe,
+    nintendo: SiNintendo,
+  };
+
+  return (
+    <HStack marginY={1}>
+      {platforms.map((platform) => (
+        <Icon as={iconMap[platform.slug]} color="gray.500" />
+      ))}
+    </HStack>
+  );
+};
+
+export default PlatformIconsList;
